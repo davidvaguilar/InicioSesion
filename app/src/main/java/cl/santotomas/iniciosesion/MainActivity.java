34b779f5cf7174e8bfdb709a1930aca2612c857d
David Villegas Aguilar <david.villegas.aguilar@gmail.com>
2020-10-06 20:27:44 -0400

    Cambios al archivo MainActivity

--- app/src/main/java/cl/santotomas/iniciosesion/MainActivity.java
+++ app/src/main/java/cl/santotomas/iniciosesion/MainActivity.java
@@ -9,48 +9,49 @@ import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("MainActivity", "Estamos en la funcion OnCreate");
+        Log.i(TAG, "Estamos en la funcion OnCreate");
         Toast.makeText(this, "Aqui el mensaje", Toast.LENGTH_LONG).show();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.i("MainActivity", "Estamos en la funcion OnStart");
+        Log.i(TAG, "Estamos en la funcion OnStart");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.i("MainActivity", "Estamos en la funcion OnResumen");
+        Log.i(TAG, "Estamos en la funcion OnResumen");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.i("MainActivity", "Estamos en la funcion OnPause");
+        Log.i(TAG, "Estamos en la funcion OnPause");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.i("MainActivity", "Estamos en la funcion OnStop");
+        Log.i(TAG, "Estamos en la funcion OnStop");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Log.i("MainActivity", "Estamos en la funcion OnRestart");
+        Log.i(TAG, "Estamos en la funcion OnRestart");
     }
 
     @Override
     protected void onDestroy() {
-        Log.i("MainActivity", "Estamos en la funcion OnDestroy");
+        Log.i(TAG, "Estamos en la funcion OnDestroy");
         super.onDestroy();
 
     }
