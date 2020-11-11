46d3ccb5525457204879316e6738f7c225d03d7f
David Villegas Aguilar <david.villegas.aguilar@gmail.com>
2020-11-10 20:00:03 -0400

    Actualizacion clase 10/11

--- app/src/main/java/cl/santotomas/iniciosesion/MainActivity.java
+++ app/src/main/java/cl/santotomas/iniciosesion/MainActivity.java
@@ -6,53 +6,41 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn_login = findViewById(R.id.btMainIngresar);
+        TextView lnk_registrar = findViewById(R.id.tvMainRegistro);
+
+        btn_login.setOnClickListener(this);
+        lnk_registrar.setOnClickListener(this);
+
         Log.i(TAG, "Estamos en la funcion OnCreate");
-        Toast.makeText(this, "Aqui el mensaje", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "Aqui el mensaje", Toast.LENGTH_LONG).show();  //NOTIFICAR AL USUARIO
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        Log.i(TAG, "Estamos en la funcion OnStart");
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btMainIngresar:
+                    Toast.makeText(this, "Se oprimio el Boton ingresar", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.tvMainRegistro:
+
+                    Intent registroIntent = new Intent(MainActivity.this, RegistroActivity.class);
+                    startActivity(registroIntent);
+                    // Toast.makeText(this, "Se oprimio", Toast.LENGTH_LONG).show();
+                break;
+        }
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.i(TAG, "Estamos en la funcion OnResumen");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.i(TAG, "Estamos en la funcion OnPause");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.i(TAG, "Estamos en la funcion OnStop");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.i(TAG, "Estamos en la funcion OnRestart");
-    }
-
-    @Override
-    protected void onDestroy() {
-        Log.i(TAG, "Estamos en la funcion OnDestroy");
-        super.onDestroy();
-
-    }
 }
--- app/src/main/java/cl/santotomas/iniciosesion/RegistroActivity.java
+++ app/src/main/java/cl/santotomas/iniciosesion/RegistroActivity.java
@@ -2,13 +2,128 @@ package cl.santotomas.iniciosesion;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class RegistroActivity extends AppCompatActivity {
+import cl.santotomas.iniciosesion.modelo.Usuario;
+
+public class RegistroActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText etRegistroNombre;
+    private EditText etRegistroEmail;
+    private EditText etRegistroClave;
+    private EditText etRegistroRepetir;
+
+    private Usuario usuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        this.etRegistroNombre = findViewById(R.id.etRegistroNombre);
+        this.etRegistroEmail = findViewById(R.id.etRegistroEmail);
+        this.etRegistroClave = findViewById(R.id.etRegistroClave);
+        this.etRegistroRepetir = findViewById(R.id.etRegistroRepetir);
+
+        this.usuario = new Usuario();
+
+        Button btRegistroConfirmar = findViewById(R.id.btRegistroConfirmar);
+        btRegistroConfirmar.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btRegistroConfirmar:
+
+                    this.usuario.setNombre(this.etRegistroNombre.getText().toString());
+                    this.usuario.setEmail(this.etRegistroEmail.getText().toString());
+                    this.usuario.setPassword(this.etRegistroClave.getText().toString());
+
+                    String repetir = this.etRegistroRepetir.getText().toString();
+
+
+                    if( !this.usuario.esVacio() ){
+                        if( this.etRegistroClave.getText().toString().length() >= 4){
+                            if( this.usuario.claveIgual(repetir) ){
+
+                                buscar_usuario();
+
+
+                                // SI USUARIO YA ESTA REGISTRADO
+                                // Consulta
+
+                                if( registrar_usuario() > 0){
+                                    this.etRegistroNombre.setText("");
+                                    this.etRegistroEmail.setText("");
+                                    this.etRegistroClave.setText("");
+                                    this.etRegistroRepetir.setText("");
+
+                                    Toast.makeText(this, "Registro del usuario correcto", Toast.LENGTH_LONG).show();
+                                    // ELIMINAR este ACTIVIDAD
+                                } else {
+                                    Toast.makeText(this, "Error al guardar en la Base de Datos", Toast.LENGTH_LONG).show();
+                                }
+                            } else {
+                                Toast.makeText(this, "Clave no es correcta", Toast.LENGTH_LONG).show();
+                            }
+                        } else {
+                            Toast.makeText(this, "La cantidad minima de caracteres son 5.", Toast.LENGTH_LONG).show();
+                        }
+                    } else {
+                        Toast.makeText(this, "Todos los campos son obligatorios", Toast.LENGTH_LONG).show();
+                    }
+
+                    //Toast.makeText(this, "Boton Registro se oprimio", Toast.LENGTH_LONG).show();
+                break;
+        }
+
+    }
+
+    private long registrar_usuario(){
+        AdminSQLiteOpenHelper base = new AdminSQLiteOpenHelper(this,"administracion1", null, 1);
+        SQLiteDatabase baseDatos = base.getWritableDatabase();
+
+        ContentValues registro = new ContentValues();
+        registro.put("nombre", this.usuario.getNombre());
+        registro.put("email", this.usuario.getEmail());
+        registro.put("clave", this.usuario.getPassword());
+
+        long resultado = baseDatos.insert("usuarios", null, registro);
+        baseDatos.close();
+
+        Log.i("PruebaRegistro", String.valueOf(resultado) );
+        return resultado;
+    }
+
+    // Funcion Validar Correo ya ingresado
+    private boolean buscar_usuario(){
+        AdminSQLiteOpenHelper base = new AdminSQLiteOpenHelper(this,"administracion1", null, 1);
+        SQLiteDatabase baseDatos = base.getWritableDatabase();
+
+        Cursor filas = baseDatos.rawQuery(
+                "SELECT COUNT(*) FROM usuarios WHERE email ="+ this.usuario.getEmail(),null
+        );
+
+        if( filas.moveToFirst() ){
+            String cantidad = filas.getString(0);
+            Log.i("LOGPRUEBA", cantidad);
+            return true;
+        } else {
+            Log.i("LOGPRUEBA", "NO existen valores igual a "+this.usuario.getEmail());
+            return false;
+        }
+
     }
 }
